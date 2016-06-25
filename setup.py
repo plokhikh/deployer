b60d376901a1b6f535a48746acf7635312523fee
Sergey Plokhikh <sergey.plokhikh@iqoption.com>
2016-06-25 15:14:14 +0300

    fixes for correct package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='Target Process python client',
+      version='0.1',
+      author='Sergey Plokhikh',
+      author_email='sergey.plokhikh@iqoption.com',
+      license='MIT',
+      description='This package allow get user stories and move it on board',
+      py_modules=['tp_connector'])
